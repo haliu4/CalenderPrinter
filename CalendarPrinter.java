d86f1c8431d3279febcb6ad1265b5f199ed6ec8c
haliu4 <haliu4@galileoweb.org>
2015-08-20 18:52:21 +0000

    This is your commit message.

---
+++ CalendarPrinter.java
@@ -0,0 +1,11 @@
+public class CalendarPrinter
+{
+    public static void main (String[] args) {
+        int x = 1;
+        while (x < 31)
+        {
+            System.out.println( "Counting September" + x );
+            x++;
+        }
+    }
+}
