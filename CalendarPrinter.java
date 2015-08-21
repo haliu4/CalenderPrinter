1063856c1be2575198270dbc19e57d49c3c758ed
haliu4 <haliu4@galileoweb.org>
2015-08-21 19:18:10 +0000

    This is your commit message.

--- CalendarPrinter.java
+++ CalendarPrinter.java
@@ -4,7 +4,22 @@ public class CalendarPrinter
         int x = 1;
         while (x < 31)
         {
-            System.out.println( "Counting September" + x );
+            if (x==5||x==6||x==12||x==13||x==19||x==20||x==26||x==27)
+            {
+                System.out.println("September " + x + " - Weekend");
+            }
+            else if (x==7)
+            {
+                System.out.println("September " + x + " - No School (Labor Day)");
+            }
+            else if (x==25)
+            {
+                System.out.println("September " + x + " -End of the marking period");
+            }
+            else
+            {
+                System.out.println("September " + x );
+            }
             x++;
         }
     }
